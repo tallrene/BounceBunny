b5d8322b914ce3e401874d631cf5d788ea1e9250
tallrene <renegaikwad@gmail.com>
2022-03-02 00:36:02 +0530

    Xcode Bounce Bunny
    Test using git

---
+++ Xcode Project/Bounce Bunny/Bounce Bunny/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bounce Bunny
+//
+//  Created by Tech Support on 02/03/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
