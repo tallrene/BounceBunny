b4382efda008ed05d4be2518897c8512c4016a95
tallrene <renegaikwad@gmail.com>
2022-03-02 01:14:58 +0530

    backup

--- Xcode Project/Bounce Bunny/Bounce Bunny/AppDelegate.swift
+++ Xcode Project/Bounce Bunny/Bounce Bunny/AppDelegate.swift
@@ -9,17 +9,62 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
+    let popover = NSPopover()
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+    @objc func printQuote(_ sender: Any?) {
+      let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+      let quoteAuthor = "Mark Twain"
+      
+      print("\(quoteText) — \(quoteAuthor)")
+    }
+    @objc func togglePopover(_ sender: Any?) {
+      if popover.isShown {
+        closePopover(sender: sender)
+      } else {
+        showPopover(sender: sender)
+      }
+    }
+
+    func showPopover(sender: Any?) {
+      if let button = statusItem.button {
+        popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+      }
+    }
+
+    func closePopover(sender: Any?) {
+      popover.performClose(sender)
+    }
 
     
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        func applicationDidFinishLaunching(_ aNotification: Notification) {
+          if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+            button.action = #selector(togglePopover(_:))
+          }
+          popover.contentViewController = BBViewController.freshController()
+        }
+
+
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
+    //Function
+    func constructMenu() {
+      let menu = NSMenu()
+
+      menu.addItem(NSMenuItem(title: "Bounce", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: "B"))
+      menu.addItem(NSMenuItem.separator())
+      menu.addItem(NSMenuItem(title: "Quit Bounce Bunny", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+
+      statusItem.menu = menu
+    }
+
 
 
 }
---
+++ Xcode Project/Bounce Bunny/Bounce Bunny/BBViewController.swift
@@ -0,0 +1,32 @@
+//
+//  BBViewController.swift
+//  Bounce Bunny
+//
+//  Created by Tech Support on 02/03/22.
+//
+
+import Cocoa
+
+class BBViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        extension BBViewController {
+          // MARK: Storyboard instantiation
+          static func freshController() -> BBViewController {
+            //1.
+            let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
+            //2.
+            let identifier = NSStoryboard.SceneIdentifier(rawValue: "BBViewController")
+            //3.
+            guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? BBViewController else {
+              fatalError("Why cant i find QuotesViewController? - Check Main.storyboard")
+            }
+            return viewcontroller
+          }
+        }
+
+    }
+    
+}
